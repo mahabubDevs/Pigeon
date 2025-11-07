7d59b2d7bcc3a8cd0bbd3fd4e234e2423fbb1ddd
mahabubDevs <mdmahabubrahman800@gmail.com>
2025-11-07 18:38:17 +0600

    upadkjfka

--- src/config/stripe.ts
+++ src/config/stripe.ts
@@ -2,8 +2,7 @@ import Stripe from 'stripe';
 import config from '.';
 
 const stripe = new Stripe(config.stripe.stripeSecretKey as string, {
-   apiVersion: "2025-10-29.clover"
-
+   apiVersion: '2025-09-30.clover'
 
 });
 
